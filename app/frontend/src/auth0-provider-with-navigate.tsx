be1f1971900ad3a8ef0676170f3977d3dc0f6d97
Jed Walton <jedwaltondev@gmail.com>
2023-06-11 16:51:06 +0100

    +

--- app/frontend/src/auth0-provider-with-navigate.tsx
+++ app/frontend/src/auth0-provider-with-navigate.tsx
@@ -20,9 +20,6 @@ export const Auth0ProviderWithNavigate = ({
     navigate(appState?.returnTo || window.location.pathname);
   };
 
-  console.log("Environment variables:")
-  console.log(domain, clientId, redirectUri, audience)
-
   if (!(domain && clientId && redirectUri && audience)) {
     return null;
   }
