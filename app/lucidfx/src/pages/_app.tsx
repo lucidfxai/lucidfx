0ced57a9bc2d4fef09cc55b371bd58712daccbf7
Jed Walton <jedwaltondev@gmail.com>
2023-06-24 15:41:31 +0100

    added trpc minimal next example

--- app/lucidfx/src/pages/_app.tsx
+++ app/lucidfx/src/pages/_app.tsx
@@ -1,9 +1,8 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-export default api.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp);
