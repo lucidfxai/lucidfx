d4fa085957cbc7ea6c93eedfe130525aa71a8c5a
Jed Walton <jedwaltondev@gmail.com>
2023-06-25 16:23:35 +0100

    basic connection - untested with PlanetScale

---
+++ app/lucidfx/src/db/connection.ts
@@ -0,0 +1,21 @@
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+
+let db: ReturnType<typeof drizzle> | undefined;
+let connection: ReturnType<typeof connect> | undefined; 
+
+export function getDb() {
+  try {
+    if (!db) {
+      connection = connect({
+        host: process.env.DATABASE_HOST,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+      });
+      db = drizzle(connection);
+    }
+  } catch (error) {
+      console.error('Failed to connect to the database: ', error);
+  }
+  return db;
+}
