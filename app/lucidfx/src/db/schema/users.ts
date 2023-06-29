9a69f0c70658326914ec8a147e8dce8c5c9476ac
Jed Walton <jedwaltondev@gmail.com>
2023-06-29 17:31:18 +0100

    Added read/write for users schema

--- app/lucidfx/src/db/schema/users.ts
+++ app/lucidfx/src/db/schema/users.ts
@@ -1,5 +1,7 @@
 import { InferModel } from 'drizzle-orm';
 import { index, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core';
+import getDb from '../connection';
+import { MySqlRawQueryResult } from 'drizzle-orm/mysql2';
  
 export const users = mysqlTable('users', {
   id: serial('id').primaryKey(),
@@ -11,3 +13,13 @@ export const users = mysqlTable('users', {
  
 export type User = InferModel<typeof users>;
 export type NewUser = InferModel<typeof users, 'insert'>; // insert type
+
+export async function insertUser(user: NewUser): Promise<MySqlRawQueryResult> {
+  const db = getDb();
+  return await db.insert(users).values(user);
+}
+
+export async function fetchUsers(): Promise<User[]> {
+  const db = getDb();
+  return await db.select().from(users);
+}
