c819b928fa77969ad1e40ebb0cb2c6ea7714f4dd
Jed Walton <jedwaltondev@gmail.com>
2023-07-03 21:27:23 +0100

    Coverage

--- app/lucidfx/integration_tests/db/schema/users.integration.test.ts
+++ app/lucidfx/integration_tests/db/schema/users.integration.test.ts
@@ -1,7 +1,8 @@
 // integrationTests/userModel.test.ts
 
+import { ResultSetHeader } from 'mysql2';
 import { endDb } from '../../../src/db/connection';
-import { insertUser, fetchUsers, NewUser, User } from '../../../src/db/schema/users'
+import { insertUser, fetchUsers, NewUser, User, deleteUser } from '../../../src/db/schema/users'
 
 describe('User model integration tests', () => {
   let newUser: NewUser;
@@ -28,18 +29,19 @@ describe('User model integration tests', () => {
     expect(userExists).toBe(true);
   });
 
-  afterAll(async () => {
-    endDb();
+
+  test('deletes a user from the database', async () => {
+    if (typeof newUser.user_id === 'string') {
+      const result = await deleteUser(newUser.user_id);
+      console.log(result);
+      const resultSet = result[0] as ResultSetHeader;
+      expect(resultSet.affectedRows).toBe(1);
+    } else {
+      throw new Error('User id is not defined');
+    }
   });
 
-  // test('deletes a user from the database', async () => {
-  //   if (typeof newUser.user_id === 'string') { // Checking if newUser.user_id is not null or undefined
-  //     const result = await deleteUser(newUser.user_id) as any; // Temporarily casting result to any
-  //     expect(result.affectedRows).toBe(1); // Assuming result has a property affectedRows
-  //   } else {
-  //     throw new Error('User id is not defined'); // Throwing an error if newUser.user_id is not a string
-  //   }
-  // });
+
   //
   //
   // test('verifies the user is deleted from the database', async () => {
@@ -48,5 +50,8 @@ describe('User model integration tests', () => {
   //   const userExists = users.some(user => user.user_id === newUser.user_id);
   //   expect(userExists).toBe(false);
   // });
+  afterAll(async () => {
+    endDb();
+  });
 });
 
