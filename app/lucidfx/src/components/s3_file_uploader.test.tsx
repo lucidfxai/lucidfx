4572b0aa59e0a5c34b9dfa0cabaff75a50d656ac
Jed Walton <jedwaltondev@gmail.com>
2023-07-18 00:18:37 +0100

    unit tests added upload

--- app/lucidfx/src/components/s3_file_uploader.test.tsx
+++ app/lucidfx/src/components/s3_file_uploader.test.tsx
@@ -1,17 +1,21 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FileUploader from './s3_file_uploader';
 import React from 'react';
 import { TRPCClientErrorLike } from '@trpc/client';
 
-
 const mockGetSignedPutUrlPromiseMutation = {
   mutate: jest.fn(),
+  data: { url: 'http://example.com', uniqueKey: 'UniqueKey123' },  // Add a mock response
 };
 
 const mockAddFileToFilesTableDbPromiseMutation = {
   mutate: jest.fn(),
 };
+
+jest.mock('../services/s3_file_uploader_service', () => ({
+  uploadFile: jest.fn().mockResolvedValue(true),
+}));
 
 jest.mock('../utils/api', () => {
   const actual = jest.requireActual('../utils/api');
@@ -54,14 +58,19 @@ describe('FileUploader', () => {
   it('uploads a file when selected', async () => {
     const MOCK_FILE = new File(['hello'], 'hello.png', { type: 'image/png' });
 
-    render(<FileUploader onUploadError={function(error: TRPCClientErrorLike<any>): void {
+    render(<FileUploader onUploadError={(error: TRPCClientErrorLike<any>): void => {
         throw new Error('Function not implemented.');
     } } />);
 
     const fileInput = screen.getByLabelText(/choose file to upload/i);
     userEvent.upload(fileInput, MOCK_FILE);
 
+    // Wait for getSignedUrlPromiseMutation.mutate to be called
     await waitFor(() => expect(mockGetSignedPutUrlPromiseMutation.mutate).toHaveBeenCalled());
+
+    // Then check if addFileToFilesTableDbPromiseMutation.mutate was called
+    await waitFor(() => expect(mockAddFileToFilesTableDbPromiseMutation.mutate).toHaveBeenCalled());
 
   });
 });
+
